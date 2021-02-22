aa7fc0e217410962a2a245fcb0234257333d062e
PKopel <pawel.kopel2@gmail.com>
2021-02-23 00:22:26 +0100

    add code from exercises

--- src/Eval.hs
+++ src/Eval.hs
@@ -19,13 +19,20 @@ apply func args = maybe (Bool False) ($ args) $ Map.lookup func primitives
 
 primitives :: Map String ([LispVal] -> LispVal)
 primitives =
-  [ ("+"        , numericBinop (+))
-  , ("-"        , numericBinop (-))
-  , ("*"        , numericBinop (*))
-  , ("/"        , numericBinop (/))
-  , ("modulo"   , intBinop mod)
-  , ("quotient" , intBinop quot)
-  , ("remainder", intBinop rem)
+  [ ("+"             , numericBinop (+))
+  , ("-"             , numericBinop (-))
+  , ("*"             , numericBinop (*))
+  , ("/"             , numericBinop (/))
+  , ("modulo"        , intBinop mod)
+  , ("quotient"      , intBinop quot)
+  , ("remainder"     , intBinop rem)
+  , ("symbol?"       , unaryOp symbolp)
+  , ("string?"       , unaryOp stringp)
+  , ("number?"       , unaryOp numberp)
+  , ("bool?"         , unaryOp boolp)
+  , ("list?"         , unaryOp listp)
+  , ("symbol->string", unaryOp sym2str)
+  , ("string->symbol", unaryOp str2sym)
   ]
 
 numericBinop :: (NumType -> NumType -> NumType) -> [LispVal] -> LispVal
@@ -39,3 +46,24 @@ intBinop op params = Number . Integer $ foldl1 op $ map unpackInt params
  where
   unpackInt (Number (Integer n)) = n
   unpackInt _                    = 0
+
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp op [val] = op val
+unaryOp _  _     = undefined
+
+symbolp, numberp, stringp, boolp, listp, sym2str, str2sym :: LispVal -> LispVal
+symbolp (Atom _) = Bool True
+symbolp _        = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+stringp (String _) = Bool True
+stringp _          = Bool False
+boolp (Bool _) = Bool True
+boolp _        = Bool False
+listp (List _        ) = Bool True
+listp (DottedList _ _) = Bool False
+listp _                = Bool False
+sym2str (Atom sym) = String sym
+sym2str _          = String ""
+str2sym (String str) = Atom str
+str2sym _            = Atom ""
