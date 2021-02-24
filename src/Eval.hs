d4319890d10def44824704ce461a7529b333b11d
PKopel <pawel.kopel2@gmail.com>
2021-02-24 16:41:23 +0100

    add case and cond

--- src/Eval.hs
+++ src/Eval.hs
@@ -6,10 +6,14 @@ module Eval where
 
 import           Import
 import qualified Data.Map                      as Map
+import           Data.List                      ( head
+                                                , tail
+                                                , last
+                                                )
 import           Control.Monad.Except           ( MonadError(throwError) )
 import           Primitives
 import           Util
-import           Quote
+import           Quote                          ( lisp )
 
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _)                   = return val
@@ -20,12 +24,32 @@ eval [lisp| (if ?pred ?conseq ?alt)|] = eval pred >>= \case
   Bool False -> eval alt
   Bool True  -> eval conseq
   _          -> throwError $ TypeMismatch "bool" pred
+eval form@(List (Atom "cond" : clauses))
+  | null clauses = throwError
+  $ BadSpecialForm "no true clause in cond expression: " form
+  | otherwise = case head clauses of
+    [lisp| (else ?expr)|] -> eval expr
+    [lisp| (?test ?expr)|] -> eval [lisp| (if ?test ?expr ?alt)|]
+    _ -> throwError $ BadSpecialForm "ill-formed cond expression: " form
+  where alt = List (Atom "cond" : tail clauses)
+eval form@(List (Atom "case" : key : clauses))
+  | null clauses = throwError
+  $ BadSpecialForm "no true clause in case expression: " form
+  | otherwise = case head clauses of
+    List (Atom "else"   : exprs) -> mapM eval exprs <&> last
+    List ((List datums) : exprs) -> do
+      result   <- eval key
+      equality <- mapM (\x -> return . Bool $ x == result) datums
+      if Bool True `elem` equality
+        then mapM eval exprs <&> last
+        else eval $ List (Atom "case" : key : tail clauses)
+    _ -> throwError $ BadSpecialForm "ill-formed case expression: " form
 eval (List (Atom func : args)) = mapM eval args >>= apply func
-eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+eval badForm = throwError $ BadSpecialForm "unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args =
-  maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
+  maybe (throwError $ NotFunction "unrecognized primitive function args" func)
         ($ args)
     $ Map.lookup func primitives
 
